8ce9b94b052c5826ff4f33f68fd42f476f271c6c
saedjabali <saad.jabali@yahoo.com>
2021-06-29 22:43:40 +0300

    kab3

--- basiclibrary/app/src/main/java/basiclibrary/App.java
+++ basiclibrary/app/src/main/java/basiclibrary/App.java
@@ -3,6 +3,16 @@
  */
 package basiclibrary;
 
+// import java.util.Arrays;
+// import java.util.Collection;
+// import java.util.Map;
+// import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) {
         int[] dices = roll(6);
@@ -21,6 +31,20 @@ public class App {
         for (int i = 0; i < mainArr.length; i++) {
             System.out.println(mainArr[i]);
         }
+        weather(weeklyMonthTemperatures);
+        // Lab03 --- most votes.
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        tally(votes);
     }
     public static int[] roll(int num) {
         int[] dice = new int[num];
@@ -43,7 +67,7 @@ public class App {
         return false;
     }
     public static double calculatingAverages(int[] arr) {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
@@ -65,5 +89,73 @@ public class App {
             }
         }
         return tempArr;
+    }
+    public static void weather(int[][] arr) {
+
+        // Add all numbers into on array
+        List<Integer> ArrayWithAllNumbers = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                ArrayWithAllNumbers.add(arr[i][j]);
+            }
+        }
+
+        // Get rid of all the duplicated values
+        Set<Integer> uniqueTemp = new HashSet<>();
+        uniqueTemp.addAll(ArrayWithAllNumbers);
+
+        // Find the min mix values
+        // store the length of the ArrayList in variable n
+        int n = ArrayWithAllNumbers.size();
+
+        int min = ArrayWithAllNumbers.get(0);
+        int max = ArrayWithAllNumbers.get(0);
+        // loop to find minimum from ArrayList
+        for (int i = 1; i < n; i++) {
+            if (ArrayWithAllNumbers.get(i) < min) {
+                min = ArrayWithAllNumbers.get(i);
+            }
+            if (ArrayWithAllNumbers.get(i) > max) {
+                max = ArrayWithAllNumbers.get(i);
+            }
+        }
+        System.out.println("This is the minimum number: " + min);
+        System.out.println("This is the maximum number: " + max);
+
+        // Never saw temperature
+        for (int i = min; i < max; i++) {
+            if (!ArrayWithAllNumbers.contains(i)) {
+                System.out.println("Never saw temperature: " + i);
+            }
+        }
+
+        System.out.println(uniqueTemp);
+        System.out.println("This is the length of the hashSet " + uniqueTemp.size());
+    }
+
+    // creating the tally function
+    public static void tally(List<String> votes) {
+        // declear a string value to give it the name with highest votes
+        String winner = "";
+
+        // declear an integer value to compare the votes
+        int temp = 0;
+
+        // declear an array list to get rid of the dublicated value
+        Set<String> getRidOfdub = new HashSet<>();
+        getRidOfdub.addAll(votes);
+
+        for (String vote : getRidOfdub) {
+
+            int highiestVotesNumber = Collections.frequency(votes, vote);
+
+            if (highiestVotesNumber > temp) {
+                temp = highiestVotesNumber;
+                winner = vote;
+            }
+        }
+
+        System.out.println(winner + " received the most votes!");
     }
 }
