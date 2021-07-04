b4d9439209d67772a1599d49f04ff2608c53ca1b
saedjabali <saad.jabali@yahoo.com>
2021-07-04 22:44:56 +0300

    lab6

---
+++ inheritance/lib/src/main/java/Demo/Review.java
@@ -0,0 +1,12 @@
+package Demo;
+
+
+public interface Review{
+    String body = null;
+    String author = null;
+    int star=0;
+
+//    default void reviewCon(String body,String author,int star)
+
+    String toString();
+}
